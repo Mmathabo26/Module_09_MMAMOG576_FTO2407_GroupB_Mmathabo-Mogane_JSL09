a826069fdacedf11113dd481f311087e27261175
Mmathabo Mogane <mmathaboappreciate@gmail.com>
2024-10-01 11:55:49 +0200

    fixed url

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 
 .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.full})`
+        
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
+
+    .catch(err => {
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+
+        document.getElementById("author").textContent = `By: Dodi Achmad`    
+    })
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))    

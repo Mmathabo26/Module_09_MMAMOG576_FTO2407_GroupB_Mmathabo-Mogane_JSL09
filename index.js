b46aab1789d995f12b8f9ce21ae76413ba42e5a4
Mmathabo Mogane <mmathaboappreciate@gmail.com>
2024-10-01 09:46:54 +0200

    fixed my url

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-fetch("https://images.unsplash.com/photo-1726502102615-07a77713dade?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
 
 .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById("author").textContent = `By: ${data.user.name}`
     })
